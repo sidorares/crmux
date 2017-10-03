8fa4124198224dea363d91d1d6e5130475bcebde
Andrey Sidorov <sidorares@yandex.com>
2017-10-03 12:09:53 +1100

    update outdated dependencies #10

--- crmux.js
+++ crmux.js
@@ -26,53 +26,59 @@ var consoleMessageEvents = [];
 var currentPausedEvent = null;
 var scriptParsedEvents = [];
 
-function replaceHost(replaceUrl, res){
+function replaceHost(replaceUrl, res) {
   var urlParsed = url.parse(replaceUrl, true);
   urlParsed.port = program.listen;
   delete urlParsed.host; //Host includes port. We want to use hostname
-  if(res){
+  if (res) {
     urlParsed.hostname = res.connection.localAddress; //Echo back the ip the client used. Supports IPv6.
   }
   return url.format(urlParsed);
 }
 
 var cacheJson = function(res) {
-  return http.request({
-    port: program.port,
-    path: '/json'
-  }, function(upRes) {
-    upRes.pipe(bl(function(err, data) {
-      var tabs = JSON.parse(data.toString());
-      var wsUrl, feUrl;
-      for (var i = 0; i < tabs.length; ++i) {
-        wsUrl = tabs[i].webSocketDebuggerUrl;
-        if (typeof wsUrl == 'undefined') {
-          wsUrl = cachedWsUrls[tabs[i].id];
-        }
-        if (typeof wsUrl == 'undefined')
-          continue;
+  return http.request(
+    {
+      port: program.port,
+      path: '/json'
+    },
+    function(upRes) {
+      upRes.pipe(
+        bl(function(err, data) {
+          var tabs = JSON.parse(data.toString());
+          var wsUrl, feUrl;
+          for (var i = 0; i < tabs.length; ++i) {
+            wsUrl = tabs[i].webSocketDebuggerUrl;
+            if (typeof wsUrl == 'undefined') {
+              wsUrl = cachedWsUrls[tabs[i].id];
+            }
+            if (typeof wsUrl == 'undefined') continue;
 
-        feUrl = tabs[i].devtoolsFrontendUrl;
+            feUrl = tabs[i].devtoolsFrontendUrl;
 
-        if (typeof feUrl == 'undefined') {
-          feUrl = cachedDevFrontendUrl[tabs[i].id];
-        }
-        if (typeof feUrl == 'undefined')
-          continue;
+            if (typeof feUrl == 'undefined') {
+              feUrl = cachedDevFrontendUrl[tabs[i].id];
+            }
+            if (typeof feUrl == 'undefined') continue;
 
-        tabs[i].webSocketDebuggerUrl = replaceHost(wsUrl, res);
-        if (tabs[i].devtoolsFrontendUrl)
-          tabs[i].devtoolsFrontendUrl = tabs[i].devtoolsFrontendUrl.replace(wsUrl.slice(5), tabs[i].webSocketDebuggerUrl.slice(5));
-        //console.log("Tabs: " + tabs[i].devtoolsFrontendUrl, wsUrl, tabs[i].webSocketDebuggerUrl);
-        cachedWsUrls[tabs[i].id] = wsUrl;
-        cachedDevFrontendUrl[tabs[i].id] = feUrl;
-      }
-      if (res) {
-        res.end(JSON.stringify(tabs));
-      }
-    }));
-  });
-}
+            tabs[i].webSocketDebuggerUrl = replaceHost(wsUrl, res);
+            if (tabs[i].devtoolsFrontendUrl)
+              tabs[i].devtoolsFrontendUrl = tabs[i].devtoolsFrontendUrl.replace(
+                wsUrl.slice(5),
+                tabs[i].webSocketDebuggerUrl.slice(5)
+              );
+            //console.log("Tabs: " + tabs[i].devtoolsFrontendUrl, wsUrl, tabs[i].webSocketDebuggerUrl);
+            cachedWsUrls[tabs[i].id] = wsUrl;
+            cachedDevFrontendUrl[tabs[i].id] = feUrl;
+          }
+          if (res) {
+            res.end(JSON.stringify(tabs));
+          }
+        })
+      );
+    }
+  );
+};
 var server = http.createServer(function(req, res) {
   if (req.url == '/json') {
     cacheJson(res).end();
@@ -80,183 +86,185 @@ var server = http.createServer(function(req, res) {
     var options = {};
     options.port = program.port;
     options.path = req.url;
-    http.request(options, function(upRes) {
-      if(req.url == '/json/list'){
-        upRes.on('data', function(data){
-          console.log('getting data');
-          if(data){
-            //console.log(data);
-            var tabs = JSON.parse(data);
-            //console.log(tabs);
-            tabs.map((tab) => {
-              //console.log(JSON.stringify(tab))
-              //console.log(cachedWsUrls[tab.id])
-              if(cachedWsUrls[tab.id]){
-                tab.webSocketDebuggerUrl = replaceHost(cachedWsUrls[tab.id], res);
-                tab.devtoolsFrontendUrl = replaceHost(cachedDevFrontendUrl[tab.id], res);
-              } else {
-                if ( typeof tab.webSocketDebuggerUrl !== 'undefined') {
-                  tab.webSocketDebuggerUrl = replaceHost(tab.webSocketDebuggerUrl, res);
+    http
+      .request(options, function(upRes) {
+        if (req.url == '/json/list') {
+          upRes.on('data', function(data) {
+            console.log('getting data');
+            if (data) {
+              //console.log(data);
+              var tabs = JSON.parse(data);
+              //console.log(tabs);
+              tabs.map(tab => {
+                //console.log(JSON.stringify(tab))
+                //console.log(cachedWsUrls[tab.id])
+                if (cachedWsUrls[tab.id]) {
+                  tab.webSocketDebuggerUrl = replaceHost(cachedWsUrls[tab.id], res);
+                  tab.devtoolsFrontendUrl = replaceHost(cachedDevFrontendUrl[tab.id], res);
+                } else {
+                  if (typeof tab.webSocketDebuggerUrl !== 'undefined') {
+                    tab.webSocketDebuggerUrl = replaceHost(tab.webSocketDebuggerUrl, res);
+                  }
+                  if (typeof tab.devtoolsFrontendUrl !== 'undefined') {
+                    tab.devtoolsFrontendUrl = replaceHost(tab.devtoolsFrontendUrl, res);
+                  }
                 }
-                if ( typeof tab.devtoolsFrontendUrl !== 'undefined') {
-                  tab.devtoolsFrontendUrl = replaceHost(tab.devtoolsFrontendUrl, res);
-                }
-              }
-            });
-            res.write(JSON.stringify(tabs));
-            upRes.pipe(res);
-          } else {
-            upRes.pipe(res);
-          }
-        });
-      } else {
-        upRes.pipe(res);
-      }
-    }).end();
+              });
+              res.write(JSON.stringify(tabs));
+              upRes.pipe(res);
+            } else {
+              upRes.pipe(res);
+            }
+          });
+        } else {
+          upRes.pipe(res);
+        }
+      })
+      .end();
   }
 });
 
 server.listen(program.listen);
 
-var wss = new WebSocket.Server({server: server});
-wss.on('connection', function(ws) {
-    var jsonReq = cacheJson();
-    jsonReq.end();
+var wss = new WebSocket.Server({ server: server });
+wss.on('connection', function(ws, request) {
+  var jsonReq = cacheJson();
+  jsonReq.end();
 
-    jsonReq.on('close', function() {
-      if (program.debug) {
-        console.log('cachedWsUrls:', cachedWsUrls);
+  jsonReq.on('close', function() {
+    if (program.debug) {
+      console.log('cachedWsUrls:', cachedWsUrls);
+    }
+  });
+
+  ws._id = lastId++;
+
+  console.log(ws.address);
+  var urlParsed = url.parse(request.url, true);
+  urlParsed.protocol = 'ws:';
+  urlParsed.slashes = '//';
+  urlParsed.hostname = 'localhost';
+  var wsUpstreamUrlPort = program.port;
+  urlParsed.port = wsUpstreamUrlPort;
+  delete urlParsed.query;
+  delete urlParsed.search;
+  delete urlParsed.host;
+  var wsUpstreamUrl = url.format(urlParsed);
+  var upstreamSocket;
+  if (!upstreamMap[wsUpstreamUrl]) {
+    upstreamSocket = new WebSocket(wsUpstreamUrl);
+    upstreamMap[wsUpstreamUrl] = {
+      localId: 0,
+      socket: upstreamSocket,
+      clients: [ws],
+      localIdToRemote: {}
+    };
+    upstreamSocket.on('message', function(message) {
+      var msgObj = JSON.parse(message);
+      if (!msgObj.id) {
+        // this is an event, broadcast it
+        upstreamMap[wsUpstreamUrl].clients.forEach(function(s) {
+          if (program.debug) console.log('e> ' + message.cyan);
+          s.send(message, function(err) {});
+        });
+
+        switch (msgObj.method) {
+          case 'Debugger.paused':
+            currentPausedEvent = message;
+            break;
+
+          case 'Debugger.resumed':
+            currentPausedEvent = null;
+            break;
+
+          case 'Debugger.scriptParsed':
+            scriptParsedEvents.push(message);
+            break;
+
+          case 'Console.messageAdded':
+          case 'Runtime.consoleAPICalled':
+            consoleMessageEvents.push(message);
+
+            // Unlike script parsed events, console messages
+            // don't represent critical state needed for clients
+            // to function properly, and therefore, to prevent
+            // unneccessary memory consumption, we can maintain a small
+            // history window, by pruning older log messages as needed.
+            if (consoleMessageEvents.length > CONSOLE_HISTORY_SIZE) {
+              consoleMessageEvents.shift();
+            }
+            break;
+        }
+      } else {
+        var idMap = upstreamMap[wsUpstreamUrl].localIdToRemote[msgObj.id];
+        delete upstreamMap[wsUpstreamUrl].localIdToRemote[msgObj.id];
+        msgObj.id = idMap.id;
+        try {
+          idMap.client.send(JSON.stringify(msgObj));
+        } catch (err) {
+          console.log('e>' + err);
+        }
+        if (program.debug) {
+          console.log(String(idMap.client._id).blue + '> ' + idMap.message.yellow);
+          console.log(String(idMap.client._id).blue + '> ' + JSON.stringify(msgObj).green);
+        }
       }
     });
+  } else {
+    upstreamSocket = upstreamMap[wsUpstreamUrl].socket;
+    upstreamMap[wsUpstreamUrl].clients.push(ws);
+  }
 
-    ws._id = lastId++;
+  ws._upstream = upstreamSocket;
+  ws._upstream.params = upstreamMap[wsUpstreamUrl];
 
-    var urlParsed = url.parse(ws.upgradeReq.url, true);
-    urlParsed.protocol = 'ws:';
-    urlParsed.slashes = '//';
-    urlParsed.hostname = 'localhost';
-    var wsUpstreamUrlPort = program.port;
-    urlParsed.port = wsUpstreamUrlPort;
-    delete urlParsed.query;
-    delete urlParsed.search;
-    delete urlParsed.host;
-    var wsUpstreamUrl = url.format(urlParsed);
-    var upstreamSocket;
-    if (!upstreamMap[wsUpstreamUrl]) {
-      upstreamSocket = new WebSocket(wsUpstreamUrl);
-      upstreamMap[wsUpstreamUrl] = {
-        localId: 0,
-        socket: upstreamSocket,
-        clients: [ws],
-        localIdToRemote: {}
-      };
-      upstreamSocket.on('message', function(message) {
-         var msgObj = JSON.parse(message);
-         if (!msgObj.id) { // this is an event, broadcast it
-           upstreamMap[wsUpstreamUrl].clients.forEach(function(s) {
-             if (program.debug)
-               console.log('e> ' + message.cyan);
-             s.send(message, function(err) {});
-           });
+  ws.on('message', function(message) {
+    var upstream = ws._upstream;
 
-           switch (msgObj.method) {
-             case "Debugger.paused":
-               currentPausedEvent = message;
-               break;
-
-             case "Debugger.resumed":
-               currentPausedEvent = null;
-               break;
-
-             case "Debugger.scriptParsed":
-               scriptParsedEvents.push(message);
-               break;
-
-             case "Console.messageAdded":
-             case "Runtime.consoleAPICalled":
-               consoleMessageEvents.push(message);
-
-               // Unlike script parsed events, console messages
-               // don't represent critical state needed for clients
-               // to function properly, and therefore, to prevent
-               // unneccessary memory consumption, we can maintain a small
-               // history window, by pruning older log messages as needed.
-               if (consoleMessageEvents.length > CONSOLE_HISTORY_SIZE) {
-                 consoleMessageEvents.shift();
-               }
-               break;
-           }
-         } else {
-           var idMap = upstreamMap[wsUpstreamUrl].localIdToRemote[msgObj.id];
-           delete upstreamMap[wsUpstreamUrl].localIdToRemote[msgObj.id];
-           msgObj.id = idMap.id;
-           try {
-             idMap.client.send(JSON.stringify(msgObj));
-           } catch (err) {
-             console.log('e>' + err)
-           }
-           if (program.debug) {
-             console.log(String(idMap.client._id).blue + "> " + idMap.message.yellow);
-             console.log(String(idMap.client._id).blue + "> " + JSON.stringify(msgObj).green);
-           }
-         }
-      });
-    } else {
-      upstreamSocket = upstreamMap[wsUpstreamUrl].socket;
-      upstreamMap[wsUpstreamUrl].clients.push(ws);
+    var msgObj;
+    try {
+      msgObj = JSON.parse(message);
+    } catch (e) {
+      console.log(e);
+      return;
     }
-
-    ws._upstream = upstreamSocket;
-    ws._upstream.params = upstreamMap[wsUpstreamUrl];
-
-    ws.on('message', function(message) {
-        var upstream = ws._upstream;
-
-        var msgObj;
-        try {
-          msgObj = JSON.parse(message);
-        } catch(e) {
-          console.log(e);
-          return;
-        }
-        upstream.params.localId++;
-        var local = upstream.params.localId;
-        var remote = msgObj.id;
-        msgObj.id = local;
-        upstream.params.localIdToRemote[local] = {
-          client: ws,
-          id: remote,
-          message: message
-        };
-        if (upstream.readyState == 0) {
-          upstream.once('open', function() {
-            upstream.send(JSON.stringify(msgObj));
-          });
-        } else
-          upstream.send(JSON.stringify(msgObj));
-    });
-
-    var removeFromUpstreamMap = function() {
-       // TODO:
-       // var upstream = ws._upstream;
-       // for each key in upstream.params.localIdToRemote
-       // delete all keys where ws._id = key.client._id
-
-       var purged = upstreamMap[wsUpstreamUrl].clients.filter(
-         function(s) { return s._id != ws._id; }
-       );
-       upstreamMap[wsUpstreamUrl].clients = purged;
+    upstream.params.localId++;
+    var local = upstream.params.localId;
+    var remote = msgObj.id;
+    msgObj.id = local;
+    upstream.params.localIdToRemote[local] = {
+      client: ws,
+      id: remote,
+      message: message
     };
-    ws.on('close', removeFromUpstreamMap);
-    ws.on('error', removeFromUpstreamMap);
+    if (upstream.readyState == 0) {
+      upstream.once('open', function() {
+        upstream.send(JSON.stringify(msgObj));
+      });
+    } else upstream.send(JSON.stringify(msgObj));
+  });
 
-    // In order to fully initialize the new debugger client's state,
-    // replay all previously occuring script parsed, console message
-    // added and paused, so that it can "catch up" with existing clients.
-    var replayEvents = scriptParsedEvents.concat(consoleMessageEvents);
-    currentPausedEvent && replayEvents.push(currentPausedEvent);
+  var removeFromUpstreamMap = function() {
+    // TODO:
+    // var upstream = ws._upstream;
+    // for each key in upstream.params.localIdToRemote
+    // delete all keys where ws._id = key.client._id
 
-    replayEvents.forEach(function (message) {
-      ws.send(message);
+    var purged = upstreamMap[wsUpstreamUrl].clients.filter(function(s) {
+      return s._id != ws._id;
     });
+    upstreamMap[wsUpstreamUrl].clients = purged;
+  };
+  ws.on('close', removeFromUpstreamMap);
+  ws.on('error', removeFromUpstreamMap);
+
+  // In order to fully initialize the new debugger client's state,
+  // replay all previously occuring script parsed, console message
+  // added and paused, so that it can "catch up" with existing clients.
+  var replayEvents = scriptParsedEvents.concat(consoleMessageEvents);
+  currentPausedEvent && replayEvents.push(currentPausedEvent);
+
+  replayEvents.forEach(function(message) {
+    ws.send(message);
+  });
 });
